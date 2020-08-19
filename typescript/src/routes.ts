a97ec857daaf6e35e4aeb98d2a98b222da701994
Felipe Dias <felipedias.dev@gmail.com>
2020-08-18 21:58:32 -0300

    Update function helloWorld to use the createUser function call

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from "express";
 
+import createUser from "./services/CreateUser";
+
 export default function helloWorld(request: Request, response: Response) {
-  return response.json({ message: "Hello World!" });
+  const user = createUser({
+    name: "Felipe",
+    email: "felipe@rocketseat.com.br",
+    password: "123456",
+    techs: [
+      "Node.js",
+      "ReactJS",
+      "React Native",
+      { title: "Javascript", experience: 95 },
+    ],
+  });
+  return response.json(user);
 }
