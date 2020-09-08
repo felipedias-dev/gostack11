5290adc13591ac11f4c7f3e810ba3521b5d6647e
Felipe Dias <felipedias.dev@gmail.com>
2020-09-07 22:56:22 -0300

    Add implementation of user interface for dashboard

--- primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório..." />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/53960261?s=460&u=4d61258682a56949ef4ebca7ace4dba530f3a6d0&v=4"
+            alt="Felipe Dias"
+          />
+          <div>
+            <strong>rafael-lip/tindev</strong>
+            <p>
+              App baseado no Tinder voltado a conectar desenvolvedores e
+              profissionais de TI
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/53960261?s=460&u=4d61258682a56949ef4ebca7ace4dba530f3a6d0&v=4"
+            alt="Felipe Dias"
+          />
+          <div>
+            <strong>rafael-lip/tindev</strong>
+            <p>
+              App baseado no Tinder voltado a conectar desenvolvedores e
+              profissionais de TI
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/53960261?s=460&u=4d61258682a56949ef4ebca7ace4dba530f3a6d0&v=4"
+            alt="Felipe Dias"
+          />
+          <div>
+            <strong>rafael-lip/tindev</strong>
+            <p>
+              App baseado no Tinder voltado a conectar desenvolvedores e
+              profissionais de TI
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
